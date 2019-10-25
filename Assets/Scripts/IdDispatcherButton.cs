1c8b3145e76cc6d0657975a91d742baf5772055e
intsven <svenkunze99@gmail.com>
2019-10-25 13:12:49 +0200

    Import useful stuff

---
+++ Assets/Scripts/IdDispatcherButton.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+
+namespace Scripts
+{
+    public class IdDispatcherButton : ARButton
+    {
+        [SerializeField] private string _targetMethod = "onUnityButtonClicked";
+        [SerializeField] private string _targetParameter = "buttonId";
+
+#if UNITY_IOS && !UNITY_EDITOR
+            [DllImport("__Internal")]
+            private extern static void UnityButtonPressed(string name);
+#else
+        private void UnityButtonPressed(string name) { }
+#endif
+
+        void Awake()
+        {
+            AndroidManager.Initialize(); // FIXME: dirty, do not initialize here
+        }
+
+        protected override void HandleTouch()
+        {
+            Debug.Log("Button " + _targetParameter + " touched!");
+            base.HandleTouch();
+
+            if (IsOnAndroid)
+            {
+                AndroidManager.GetInstance().CallJavaFunc(_targetMethod, _targetParameter);
+            }
+            else if (IsOnIOS)
+            {
+                UnityButtonPressed(_targetParameter);
+            }
+            else
+            {
+                Debug.Log("Hit via Raycast");
+            }       
+            
+        }
+    }
+}
---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Runtime.InteropServices;
+
+namespace Scripts
+{
+    public class SceneSwitch : MonoBehaviour
+    {
+        [SerializeField] private bool _doCheckOnStart;
+
+        // ---
+#if UNITY_IOS && !UNITY_EDITOR
+            [DllImport("__Internal")]
+            private extern static int UnityRequestScene();
+#else
+        private int UnityRequestScene() { return 0; }
+#endif
+
+        protected bool IsOnAndroid { get { return Application.platform == RuntimePlatform.Android; } }
+        protected bool IsOnIOS { get { return Application.platform == RuntimePlatform.IPhonePlayer; } }
+        protected bool IsOnMobile { get { return IsOnAndroid || IsOnIOS; } }
+        // ---
+
+        void Awake()
+        {
+            AndroidManager.Initialize();
+        }
+
+        void Start()
+        {
+            if (_doCheckOnStart)
+                HandleSceneSwitch();
+        }
+
+        void OnApplicationPause(bool isPaused)
+        {
+            if (!isPaused)
+                HandleSceneSwitch();
+        }
+
+        private void HandleSceneSwitch()
+        {
+            Debug.Log("Handle Scene Switch.");
+
+            if (IsOnAndroid)
+            {
+                var sceneName = AndroidManager.GetInstance().RetrieveTargetSceneName();
+                if (SceneManager.GetActiveScene().name == sceneName) return;
+
+                if (!string.IsNullOrEmpty(sceneName))
+                    SceneManager.LoadScene(sceneName);
+                else
+                {
+                    Debug.LogError("Error: Could not retrieve target scene name from Android App!");
+                    SceneManager.LoadScene("MainScene");
+                }
+            }
+            else if (IsOnIOS)
+            {
+                int scene = UnityRequestScene();
+
+                Debug.Log("Scene: " + scene);
+
+                if (scene == 0) SceneManager.LoadScene("MainScene");
+                else if (scene == 1) SceneManager.LoadScene("3DMotorScene");
+                else if (scene == 2) SceneManager.LoadScene("TurntableScene");
+                else SceneManager.LoadScene("MainScene");
+            }
+            else
+            {
+                Debug.Log("Neither Android, nor iOS detected.");
+            }
+        }
+    }
+}
